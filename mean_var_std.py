7b6eb2174692564bc154befd1b38c14d19ee6e82
Adam Ault <awault@gmail.com>
2024-04-01 23:05:04 +0000

    Construct ValueError

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        print("ValueError: List must contain nine numbers.")
+    
+    else:
+        print("Continued")
+
+list = ([0,1,2,3,4,5,6,7,8])
 
 
 
